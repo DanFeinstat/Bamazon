be7db591cce5c9866b113c157c0fe5cfb8322411
DanFein <danfeinstat@gmail.com>
2018-04-17 16:23:39 -0700

    customer order is fully functional, want to update initial data list to look better

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,49 +10,84 @@ var con = mysql.createConnection({
   database: "bamazon",
 });
 
-// con.connect(function(err) {
-//   if (err) throw err;
-//   console.log("connected as id " + con.threadId);
-//   con.query("SELECT*FROM products", function(err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//   });
-//   con.end();
-// });
-
-inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "item_Id",
-      message: "Enter the ID number of the item you'd like to purchase.",
-    },
-    {
-      type: "unput",
-      name: "order_quantity",
-      message: "Please enter how many you'd like to purchase.",
-    },
-  ])
-  .then(function(answer) {
-    var item = answer.item_Id;
-    var quantity = answer.order_quantity;
-    orderItem(item, quantity);
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + con.threadId);
+  con.query("SELECT*FROM products", function(err, result) {
+    if (err) throw err;
+    console.log(result);
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "item_Id",
+          message: "Enter the ID number of the item you'd like to purchase.",
+        },
+        {
+          type: "unput",
+          name: "order_quantity",
+          message: "Please enter how many you'd like to purchase.",
+        },
+      ])
+      .then(function(answer) {
+        var item = answer.item_Id;
+        var quantity = answer.order_quantity;
+        orderItem(item, quantity);
+      });
   });
+  //   con.end();
+});
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       name: "item_Id",
+//       message: "Enter the ID number of the item you'd like to purchase.",
+//     },
+//     {
+//       type: "unput",
+//       name: "order_quantity",
+//       message: "Please enter how many you'd like to purchase.",
+//     },
+//   ])
+//   .then(function(answer) {
+//     var item = answer.item_Id;
+//     var quantity = answer.order_quantity;
+//     orderItem(item, quantity);
+//   });
 
 function orderItem(item, quantity) {
-  con.connect(function(err) {
+  //   con.connect(function(err) {
+  //     if (err) throw err;
+  con.query("SELECT*FROM products WHERE ?", [{ id: item }], function(
+    err,
+    result
+  ) {
     if (err) throw err;
-    con.query("SELECT*FROM products WHERE ?", [{ id: item }], function(
-      err,
-      result
-    ) {
-      if (err) throw err;
-      if (result[0].stock_quantity < quantity) {
-        console.log("Insufficient Quantity!");
-      } else {
-      }
-      console.log(result[0].stock_quantity);
-    });
+    console.log(
+      "remaining stock of " +
+        result[0].product_name +
+        ": " +
+        result[0].stock_quantity
+    );
+    if (result[0].stock_quantity < quantity) {
+      console.log("Insufficient Quantity!");
+    } else {
+      console.log(item);
+      var newStock =
+        parseFloat(result[0].stock_quantity) - parseFloat(quantity);
+      con.query(
+        "UPDATE products SET stock_quantity = " +
+          newStock +
+          " WHERE id = " +
+          item,
+        function(err, result) {
+          console.log("Thank you for your purchase!");
+        }
+      );
+    }
     con.end();
   });
+  //   });
 }
